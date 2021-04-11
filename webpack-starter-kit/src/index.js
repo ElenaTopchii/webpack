34c7e9f1ff7c191464ac59670544c1200cd87e86
ElenaTopchii <panchenko.elena.uk@gmail.com>
2021-04-11 20:13:06 +0300

    practice 10 module

--- webpack-starter-kit/src/index.js
+++ webpack-starter-kit/src/index.js
@@ -1,1 +1,125 @@
+import throttle from 'lodash.throttle';
+
 import './styles.css';
+
+const STORAGE_KEY = 'feedback-msg';
+
+const refs = {
+  form: document.querySelector('.js-feedback-form'),
+  textarea: document.querySelector('.js-feedback-form  textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 200));
+
+populateTextarea();
+
+/*
+ * - Останавливаем поведение по умолчанию
+ * - Убираем сообщение из хранилища
+ * - Очищаем форму
+ */
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log('Отправляем форму');
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+/*
+ * - Получаем значение поля
+ * - Сохраняем его в хранилище
+ * - Можно добавить throttle
+ */
+function onTextareaInput(evt) {
+  const message = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+/*
+ * - Получаем значение из хранилища
+ * - Если там что-то было, обновляем DOM
+ */
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+  }
+}
+
+// Домой
+// сделать так чтобы сохраняло не только сообщение но и имя, и все в одном обьекте
+
+// const formData = {};
+
+// refs.form.addEventListener('input', e => {
+//   // console.log(e.target.name);
+//   // console.log(e.target.value);
+
+//   formData[e.target.name] = e.target.value;
+
+//   console.log(formData);
+// });
+
+import colorCardTpl from './templates/color-card.hbs';
+import colorCardsTpl from './templates/color-cards.hbs';
+import colors from './colors.json';
+
+
+
+const paletteContainer = document.querySelector('.js-palette');
+const cardsMarkup = createColorCardsMarkup(colors);
+
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+paletteContainer.addEventListener('click', onPaletteContainerClick);
+
+function createColorCardsMarkup(colors) {
+  // return colors.map(color => colorCardTpl(color)).join('');
+  // return colors.map(colorCardTpl).join('');
+
+  return colorCardsTpl(colors);
+}
+
+function onPaletteContainerClick(evt) {
+  const isColorSwatchEl = evt.target.classList.contains('color-swatch');
+
+  if (!isColorSwatchEl) {
+    return;
+  }
+
+  const swatchEl = evt.target;
+  const parentColorCard = swatchEl.closest('.color-card');
+
+  removeActiveCardClass();
+  addActiveCardClass(parentColorCard);
+  setBodyBgColor(swatchEl.dataset.hex);
+}
+
+function setBodyBgColor(color) {
+  document.body.style.backgroundColor = color;
+}
+
+function removeActiveCardClass() {
+  const currentActiveCard = document.querySelector('.color-card.is-active');
+
+  if (currentActiveCard) {
+    currentActiveCard.classList.remove('is-active');
+  }
+}
+
+function addActiveCardClass(card) {
+  card.classList.add('is-active');
+}
+
+
+import countries from './countries.json';
+import itemsTemplate from './templates/gallery-items.hbs';
+
+const galleryRef = document.querySelector('.js-gallery');
+
+const markup = itemsTemplate(countries);
+galleryRef.insertAdjacentHTML('beforeend', markup);
